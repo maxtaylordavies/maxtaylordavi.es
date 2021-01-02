28b21050e4dbb94dbca6e93e40e65e1c5561e7c6
Max Taylor-Davies <maxtaylordavies@gmail.com>
2021-01-02 20:17:55 +0000

    adds print statement

--- repository/PostRepository.go
+++ repository/PostRepository.go
@@ -50,7 +50,7 @@ func (pr *PostRepository) All() ([]Post, error) {
 
 		var tags []string
 		meta := doc.Find("meta").Text()
-		fmt.Println(meta)
+		fmt.Println(doc)
 		i := strings.Index(meta, "name='tags'")
 		if i != -1 {
 			meta = meta[i:]
